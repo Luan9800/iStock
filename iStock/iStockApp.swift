b05f57a37eccfb0c89887ef4291a06c0fa8cc181
Luan Carlos <67272266+Luan9800@users.noreply.github.com>
2026-07-02 20:20:04 -0300

    Initial Commit

---
+++ iStock/iStockApp.swift
@@ -0,0 +1,17 @@
+//
+//  iStockApp.swift
+//  iStock
+//
+//  Created by Luan Carlos on 02/07/26.
+//
+
+import SwiftUI
+
+@main
+struct iStockApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
